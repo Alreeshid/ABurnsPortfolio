750fc8e6ee5488b6a08c7a37b3c0f6fb43491bf4
Big Burger <aburns10@ggc.edu>
2024-02-20 16:45:51 -0500

    mmmmmmm

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 //Collection of general scripts
 
-//Theory tester, animates based on how much the user has scrolled.
-const home = document.getElementById("home");
-home.addEventListener('wheel', function(event) {
-    const y = event.deltaY;
-    //element.style.transform = `translateY(${delta}px)`;
-});
+//experiment, animates based on how much the user has scrolled.
+let home = document.getElementById("home");
+window.onscroll = (e) => {
+    e.stopPropagation();
+    window.pageYOffset > 50 && console.log("do smh");
+}
