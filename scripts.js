e53802c1a72be63d57d973eb01b799e9d8f41040
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2024-02-21 16:58:08 -0500

    l

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+import anime from 'anime-master/lib/anime.es.js';
+const anime = require('animejs');
 //Collection of general scripts
 
 //experiment, animates based on how much the user has scrolled.
@@ -6,3 +8,8 @@ window.onscroll = (e) => {
     e.stopPropagation();
     window.pageYOffset > 50 && console.log("do smh");
 }
+
+anime({
+    targets: '#home',
+    translateX: 100
+});
