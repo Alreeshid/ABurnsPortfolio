a44ffcb7e9defb390d6341bd0a78d4a17969124a
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2024-07-24 10:10:26 -0400

    test 1 peoper

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
 root.render(e(LikeButton));
+
+function MyButton() {
+    return (
+      <button>I'm a button</button>
+    );
+  }
