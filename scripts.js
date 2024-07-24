61655bd966e77bec605252c029ee724238547dc8
Big Burger <110642010+Alreeshid@users.noreply.github.com>
2024-07-24 10:30:05 -0400

    1.3

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 //const domContainer = document.querySelector('#like_button_container');
 //const root = ReactDOM.createRoot(domContainer);
 //root.render(e(LikeButton));
-import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
 
 function MyButton() {
     return (
